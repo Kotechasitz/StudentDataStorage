cf6002d2c120d4dc4649bc93ba368cca556e0b82
kotechasitz <fewzanpk@gmail.com>
2019-04-13 19:19:13 +0700

    add class studenData

--- src/passclass/Source/StuData.java
+++ src/passclass/Source/StuData.java
@@ -4,22 +4,64 @@
  * and open the template in the editor.
  */
 package passclass.Source;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author 10.1803
  */
 public class StuData {
-    private String StuCode,Name,Grade,TypeClass;
     
-     public String getStuCode() {
+    private String StuCode;
+    private String Name;
+    private String Grade;
+    private String TypeClass;
+
+    private static List<String> data = new ArrayList<>();
+
+    static {
+        data.add("123,A1 B,1.00,IEP");
+        data.add("456,A2 B,1.00,IEP");
+        data.add("789,A3 B,1.00,IEP");
+    }
+
+    public void tranData(){
+        String[] arr = new String[0];
+        int cnt = 0;
+        for (int i = 0; i < data.size() ; i++) {
+            System.out.println(data.get(i));
+            arr = data.get(i).split(",");
+            
+            if (cnt == arr.length-4){
+                setStuCode(arr[0]);
+            }
+            if (cnt+1 == arr.length-3){
+                setName(arr[1]);
+            }
+            if (cnt+2 == arr.length-2){
+                setGrade(arr[2]);
+            }
+            if (cnt+3 == arr.length-1){
+                setTypeClass(arr[3]);
+            }
+
+            System.out.println(getStuCode());
+            System.out.println(getName());
+            System.out.println(getGrade());
+            System.out.println(getTypeClass());
+        }
+        System.out.println(arr.length);
+    }
+
+
+    public String getStuCode() {
         return StuCode;
     }
 
     public void setStuCode(String stuCode) {
         StuCode = stuCode;
     }
-    
+
     public String getName() {
         return Name;
     }
@@ -27,7 +69,7 @@ public class StuData {
     public void setName(String name) {
         Name = name;
     }
-    
+
     public String getGrade() {
         return Grade;
     }
@@ -35,12 +77,12 @@ public class StuData {
     public void setGrade(String grade) {
         Grade = grade;
     }
-    
+
     public String getTypeClass() {
         return TypeClass;
     }
 
-    public void setTypeClass(String typeclass) {
-        TypeClass = typeclass;
+    public void setTypeClass(String typeClass) {
+        TypeClass = typeClass;
     }
 }
